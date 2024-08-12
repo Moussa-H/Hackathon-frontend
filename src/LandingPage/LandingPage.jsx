d37d96fbaad15ca5f502049248b6eafa06259ff1
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-08-12 14:03:30 +0300

    Fixed errors related to merging

--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import React, { useState } from 'react';
 import NavBar from '../component/Navbar/NavBar'; 
 import './LandingPage.css';
 import hero from '../Images/hero-logo.png';
